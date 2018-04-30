9898e3199c3776853a7428f1905677a0de5be42a
Michael Wang <xiaoqunw@hotmail.com>
2018-04-30 14:10:58 -0400

    init checkin

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "gym-scala-client"
+
+version := "1.0"
+
+scalaVersion := "2.12.3"
+
+libraryDependencies += "io.spray" %%  "spray-json" % "1.3.3"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.12",
+  "com.typesafe.akka" %% "akka-testkit" % "2.5.12" % Test
+)
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % "2.5.12",
+  "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.12" % Test
+)
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % "10.1.1",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.1.1" % Test
+)
---
+++ src/main/scala/gym/scala/client/Client.scala
@@ -0,0 +1,41 @@
+package gym.scala.client
+
+
+import spray.json._
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, _}
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
+
+/**
+  * Created by Michael Wang on 04/26/2018.
+  */
+object Client extends App {
+
+  val source = """{ "env_id": "CartPole-v0" }"""
+  val jsonAst = source.parseJson // or JsonParser(source)
+  val json = jsonAst.prettyPrint
+
+  //printf(json)
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val http = HttpRequest(uri = "http://127.0.0.1:5000/v1/envs/")
+    .withMethod(HttpMethods.POST).withEntity(HttpEntity(ContentTypes.`application/json`,source))
+  val responseFuture: Future[HttpResponse] = Http().singleRequest(http)
+
+  responseFuture
+    .onComplete {
+      case Success(res) => println(res)
+      case Failure(_)   => sys.error("something wrong")
+    }
+
+  println(json)
+  //val jsonAst1 = List(1, 2, 3).toJson
+  //val myObject = jsonAst.convertTo[MyObjectType]
+}
