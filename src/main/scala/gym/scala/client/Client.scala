4479bef2f58a6e643154e835fa79a1a4a8d081ef
Michael Wang <xiaoqunw@hotmail.com>
2018-05-01 11:10:42 -0400

    init checkin

--- src/main/scala/gym/scala/client/Client.scala
+++ src/main/scala/gym/scala/client/Client.scala
@@ -12,29 +12,37 @@ import scala.util.{Failure, Success}
 /**
   * Created by Michael Wang on 04/26/2018.
   */
-object Client extends App {
-  val source = """{ "env_id": "CartPole-v0" }"""
-  val jsonAst = source.parseJson // or JsonParser(source)
-  val json = jsonAst.prettyPrint
+class Client(val host:String, val port:Int) {
+  val contentType = ContentTypes.`application/json`
+  val envRoot = "/v1/envs/"
 
-  //printf(json)
-
-  implicit val system: ActorSystem = ActorSystem()
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-
-  val http = HttpRequest(uri = "http://127.0.0.1:5000/v1/envs/")
-    .withMethod(HttpMethods.POST).withEntity(HttpEntity(ContentTypes.`application/json`,source))
-  val responseFuture: Future[HttpResponse] = Http().singleRequest(http)
-
-  responseFuture
-    .onComplete {
-      case Success(res) => println(res)
-        val e = res.entity
-      case Failure(_)   => sys.error("something wrong")
+  def execute(command:GymApi): Unit = {
+    command match {
+      case createEnv => printf("createEnv")
     }
+  }
 
-  println(json)
+//  val jsonAst = source.parseJson // or JsonParser(source)
+//  val json = jsonAst.prettyPrint
+//
+//  //printf(json)
+//
+//  implicit val system: ActorSystem = ActorSystem()
+//  implicit val materializer: ActorMaterializer = ActorMaterializer()
+//  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+//
+//  val http = HttpRequest(uri = "http://127.0.0.1:5000/v1/envs/")
+//    .withMethod(HttpMethods.POST).withEntity(HttpEntity(ContentTypes.`application/json`,source))
+//  val responseFuture: Future[HttpResponse] = Http().singleRequest(http)
+//
+//  responseFuture
+//    .onComplete {
+//      case Success(res) => println(res)
+//        val e = res.entity
+//      case Failure(_)   => sys.error("something wrong")
+//    }
+//
+//  println(json)
   //val jsonAst1 = List(1, 2, 3).toJson
   //val myObject = jsonAst.convertTo[MyObjectType]
 }
---
+++ src/test/scala/gym/scala/client/test/gymClientTest.scala
@@ -0,0 +1,13 @@
+package gym.scala.client.test
+
+import gym.scala.client._
+/**
+  * Created by wangmich on 05/01/2018.
+  */
+object gymClientTest extends App {
+
+   val client = new Client("127.0.0.1", 5000)
+   val createEnv = new createEnv(None, "CartPole-v0")
+   client.execute(createEnv)
+
+}
