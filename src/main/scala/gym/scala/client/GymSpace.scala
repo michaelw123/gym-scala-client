dcfdb35f512af426e301c7a9f692ce238668c391
Michael Wang <xiaoqunw@hotmail.com>
2018-06-08 15:13:14 -0400

    implement CartPole

--- src/main/scala/gym/scala/client/GymSpace.scala
+++ src/main/scala/gym/scala/client/GymSpace.scala
@@ -71,13 +71,17 @@ object GymSpace {
       var aSample = List[Double]()
       for (i <- shape(0)-1 to 0) aSample = Uniform(low(i), high(i)).draw :: aSample
       aSample
+//
+//      List(Uniform(low(0), high(0)).draw,
+//        Uniform(low(1), high(1)).draw,
+//        Uniform(low(2), high(2)).draw,
+//        Uniform(low(3), high(3)).draw)
     }
     def contains(x:List[Double]):Boolean = {
       false
     }
   }
   object BoxSpace  extends DefaultJsonProtocol with SprayJsonSupport {
-
     implicit val boxSpaceFormat = jsonFormat4(BoxSpace.apply)
   }
 
@@ -87,23 +91,23 @@ object GymSpace {
   }
 
   case class ObservationSpace (info:BoxSpace){
-    def discretize(obs:Observation, buckets:(Int, Int, Int, Int)): Observation  = {
-      val upperBound = (info.high(0), 0.5, info.high(2), scala.math.toRadians(50.0))
-      val lowerbound = (info.low(0), -0.5, info.low(2), -scala.math.toRadians(50.0))
-      val ratios = ((obs.observation(0) + scala.math.abs(lowerbound._1)) / (upperBound._1 - lowerbound._1),
-          (obs.observation(1) + scala.math.abs(lowerbound._2)) / (upperBound._2 - lowerbound._2),
-          (obs.observation(2) + scala.math.abs(lowerbound._3)) / (upperBound._3 - lowerbound._3),
-          (obs.observation(3) + scala.math.abs(lowerbound._4)) / (upperBound._4 - lowerbound._4))
-      val newObs = (scala.math.round((buckets._1 -1 ) * ratios._1),
-          scala.math.round((buckets._2 -1 ) * ratios._2),
-          scala.math.round((buckets._3 -1 ) * ratios._3),
-          scala.math.round((buckets._4 -1 ) * ratios._4))
-      val theObs:List[Double] = List(scala.math.min(buckets._1 -1, scala.math.max(0, newObs._1)),
-        scala.math.min(buckets._2 -1, scala.math.max(0, newObs._2)),
-        scala.math.min(buckets._3 -1, scala.math.max(0, newObs._3)),
-        scala.math.min(buckets._4 -1, scala.math.max(0, newObs._4)))
-      Observation(theObs)
-    }
+//    def discretize(obs:Observation, buckets:(Int, Int, Int, Int)): Observation  = {
+//      val upperBound = (info.high(0), 0.5, info.high(2), scala.math.toRadians(50.0))
+//      val lowerbound = (info.low(0), -0.5, info.low(2), -scala.math.toRadians(50.0))
+//      val ratios = ((obs.observation(0) + scala.math.abs(lowerbound._1)) / (upperBound._1 - lowerbound._1),
+//          (obs.observation(1) + scala.math.abs(lowerbound._2)) / (upperBound._2 - lowerbound._2),
+//          (obs.observation(2) + scala.math.abs(lowerbound._3)) / (upperBound._3 - lowerbound._3),
+//          (obs.observation(3) + scala.math.abs(lowerbound._4)) / (upperBound._4 - lowerbound._4))
+//      val newObs = (scala.math.round((buckets._1 -1 ) * ratios._1),
+//          scala.math.round((buckets._2 -1 ) * ratios._2),
+//          scala.math.round((buckets._3 -1 ) * ratios._3),
+//          scala.math.round((buckets._4 -1 ) * ratios._4))
+//      val theObs:List[Double] = List(scala.math.min(buckets._1 -1, scala.math.max(0, newObs._1)),
+//        scala.math.min(buckets._2 -1, scala.math.max(0, newObs._2)),
+//        scala.math.min(buckets._3 -1, scala.math.max(0, newObs._3)),
+//        scala.math.min(buckets._4 -1, scala.math.max(0, newObs._4)))
+//      Observation(theObs)
+//    }
   }
   object  ObservationSpace extends DefaultJsonProtocol with SprayJsonSupport {
     implicit val observationSpaceFormat = jsonFormat1(ObservationSpace.apply)
--- src/test/scala/gym/scala/client/test/CartPole.scala
+++ src/test/scala/gym/scala/client/test/CartPole.scala
@@ -84,18 +84,19 @@ object CartPole extends App {
   val reset = resetEnv(gymInstance)
   var gymObs = gymClient.execute(reset)
   println(gymObs)
-  var origObs = gymObsSpace.discretize(gymObs, buckets)
+    val cartPoleObsSpace:CartPoleObservationSpace = gymObsSpace
+  var origObs = cartPoleObsSpace.discretize(gymObs, buckets)
   var rewards  = List[Double]()
-  for (episode <- 1 to 800) {
+  for (episode <- 1 to 500) {
     var done = false
     thePolicy.setEpisode(episode)
-    origObs = gymObsSpace.discretize(gymClient.execute(reset), buckets)
+    origObs = cartPoleObsSpace.discretize(gymClient.execute(reset), buckets)
     for (t <- 1 to 210 if !done) {
       val action = thePolicy.chooseAction(origObs.indice)
       val step1 = step(gymInstance, action, false)
       val stepReply = gymClient.execute(step1)
       done = stepReply.done
-      val next_obs = gymObsSpace.discretize(Observation(stepReply.observation), buckets)
+      val next_obs = cartPoleObsSpace.discretize(Observation(stepReply.observation), buckets)
       thePolicy.update(origObs.indice, next_obs.indice, action, stepReply.reward)
       origObs = next_obs
       if (done) rewards = t :: rewards
@@ -131,12 +132,38 @@ object CartPole extends App {
       new CartPoleObservation(x, xdot, theta, thetadot)
     }
   }
+   case class CartPoleObservationSpace(high:(Double, Double, Double, Double), low:(Double, Double, Double, Double), name:String="", shape:Int=4) {
+     def discretize(obs:Observation, buckets:(Int, Int, Int, Int)): Observation  = {
+        val ratios = ((obs.observation(0) + scala.math.abs(low._1)) / (high._1 - low._1),
+         (obs.observation(1) + scala.math.abs(low._2)) / (high._2 - low._2),
+         (obs.observation(2) + scala.math.abs(low._3)) / (high._3 - low._3),
+         (obs.observation(3) + scala.math.abs(low._4)) / (high._4 - low._4))
+       val newObs = (scala.math.round((buckets._1 -1 ) * ratios._1),
+         scala.math.round((buckets._2 -1 ) * ratios._2),
+         scala.math.round((buckets._3 -1 ) * ratios._3),
+         scala.math.round((buckets._4 -1 ) * ratios._4))
+       val theObs:List[Double] = List(scala.math.min(buckets._1 -1, scala.math.max(0, newObs._1)),
+         scala.math.min(buckets._2 -1, scala.math.max(0, newObs._2)),
+         scala.math.min(buckets._3 -1, scala.math.max(0, newObs._3)),
+         scala.math.min(buckets._4 -1, scala.math.max(0, newObs._4)))
+       Observation(theObs)
+     }
+  }
+  implicit def gymObsSpace2CartPoleObsSpace(obsSpace: ObservationSpace):CartPoleObservationSpace = {
+   val high = (obsSpace.info.high(0), 0.5, obsSpace.info.high(2), scala.math.toRadians(50.0))
+    val low = (obsSpace.info.low(0), -0.5, obsSpace.info.low(2), scala.math.toRadians(50.0))
+    val name = obsSpace.info.name
+    val shape = obsSpace.info.shape(0)
+    CartPoleObservationSpace(high, low, name, shape)
+
+  }
   implicit def gymObs2Observation(gymObs:Observation):CartPoleObservation = {
         new CartPoleObservation(gymObs.observation(0).toInt,
           gymObs.observation(1).toInt,
           gymObs.observation(2).toInt,
           gymObs.observation(3).toInt)
   }
+
   case class cartPolePolicy ( indices:Int, actions:Int) {
     val discount = 0.99
     var episode = 0
